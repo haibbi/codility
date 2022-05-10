3806f7a0310a581e15bb6f84c6ab2953db6625ea
haibbi <hibrahimbinol@gmail.com>
2022-05-10 09:40:29 +0300

    fix cyclic rotation

--- src/main/java/com/github/haibbi/codility/CyclicRotation.java
+++ src/main/java/com/github/haibbi/codility/CyclicRotation.java
@@ -3,21 +3,13 @@ package com.github.haibbi.codility;
 public class CyclicRotation {
 
     public int[] solution(int[] A, int K) {
-        int[] result = new int[A.length];
-        int delta = A.length - K;
-        System.arraycopy(A, K + 1, result, 0, delta);
-        System.arraycopy(A, 0, result, delta, delta + 1);
-        return result;
-    }
+        int shift = K % A.length;
+        if (shift == 0) return A;
 
-    public int[] solutionMemoryEfficient(int[] A, int K) {
-        for (int i = 0; i < A.length - K; i++) {
-            int position = (i + K) % A.length;
-            int temp = A[position];
-            A[position] = A[i];
-            A[i] = temp;
-        }
-        return A;
+        int[] result = new int[A.length];
+        System.arraycopy(A, A.length - shift, result, 0, shift);
+        System.arraycopy(A, 0, result, shift, A.length - shift);
+        return result;
     }
 
 }
--- src/test/java/com/github/haibbi/codility/CyclicRotationTest.java
+++ src/test/java/com/github/haibbi/codility/CyclicRotationTest.java
@@ -1,20 +1,38 @@
 package com.github.haibbi.codility;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class CyclicRotationTest {
 
+    private CyclicRotation cyclicRotation;
+
+    @BeforeEach
+    void setUp() {
+        this.cyclicRotation = new CyclicRotation();
+    }
+
     @Test
     void test() {
-        CyclicRotation cyclicRotation = new CyclicRotation();
-        assertArrayEquals(
-                new int[]{6, 3, 8, 9, 7},
-                cyclicRotation.solution(new int[]{3, 8, 9, 7, 6}, 3)
-        );
+        int[] array = {3, 8, 9, 7, 6};
+        check(new int[]{3, 8, 9, 7, 6}, array, 0);
+        check(new int[]{6, 3, 8, 9, 7}, array, 1);
+        check(new int[]{7, 6, 3, 8, 9}, array, 2);
+        check(new int[]{9, 7, 6, 3, 8}, array, 3);
+        check(new int[]{8, 9, 7, 6, 3}, array, 4);
+        check(new int[]{3, 8, 9, 7, 6}, array, 5);
+        check(new int[]{6, 3, 8, 9, 7}, array, 6);
+        check(new int[]{7, 6, 3, 8, 9}, array, 7);
+    }
+
+    @Test
+    void test2() {
+        check(new int[]{1, 2, 3, 4}, new int[]{1, 2, 3, 4}, 4);
+    }
+
+    private void check(int[] expected, int[] array, int k) {
+        assertArrayEquals(expected, cyclicRotation.solution(array, k));
     }
 }
